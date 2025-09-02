99e4766fc45f3a177c5c64e8e17b4098e7c39023
Vivek Kumar Mahto <vivekkumarmahto101103@gmail.com>
2025-09-02 15:18:32 +0530

    OverRiding Changes

--- Inheritance/MethodOverRiding.java
+++ Inheritance/MethodOverRiding.java
@@ -18,11 +18,28 @@ class Dog extends Animal{
     }
 }
 public class MethodOverRiding {
+    static void validateAnimal(Animal x){
+        // object instanceof class
+        if(x instanceof Cat) {
+            System.out.println("It's a Cat");
+        } else {
+            System.out.println("It's a Dog");
+        }
+    }
     public static void main(String[] args) {
         Cat c1 = new Cat();
         c1.makeSound();
 
         Dog d1 = new Dog();
         d1.makeSound();
+
+        // Explain Dynamic Method Dispatch
+        // Decide which overriden method gets executed when a sub-class object is stored in the reference
+        // of super class
+        Animal a1 = new Cat();
+        Animal a2 = new Dog();
+
+        validateAnimal(a1);
+        validateAnimal(a2);
     }
 }
